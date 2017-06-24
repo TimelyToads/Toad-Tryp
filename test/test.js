6abd9280e9c619cde26df3fd72835e0df4ef2db2
Dylan Gould <dylangood@gmail.com>
2017-06-24 16:19:12 -0700

    Install mocha & chai and write an example test

---
+++ test/test.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+
+// This test is a fake example that I should remove soon:
+describe('Array', function () {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', () => {
+      assert.equal(-1, [1, 2, 3].indexOf(4));
+    });
+  });
+});
+
