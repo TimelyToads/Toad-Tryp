d9ca89dad1e5c77a7d04aaef3e76ce82f0c4a35a
Solomon Tang <solomoncztang@gmail.com>
2017-06-24 19:29:29 -0700

    Render 404 PAGE NOT FOUND if URL path does not have specified route

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import {
   BrowserRouter as Router,
+  browserHistory,
   Route,
-  Link
+  Link,
+  Switch
 } from 'react-router-dom'
 
 import Home from './Home.jsx';
+import Login from './Login.jsx';
+import Signup from './Signup.jsx';
+import NotFound from './NotFound.jsx';
 // Each logical "route" has two components, one for
 // the sidebar and one for the main area. We want to
 // render both of them in different places when the
@@ -13,40 +18,43 @@ import Home from './Home.jsx';
 const routes = [
   { path: '/',
     exact: true,
-    sidebar: () => <div>home!</div>,
     main: Home
   },
-  { path: '/bubblegum',
-    sidebar: () => <div>bubblegum!</div>,
-    main: () => <h2>Bubblegum</h2>
+  { path: '/login',
+    main: Login
   },
-  { path: '/shoelaces',
-    sidebar: () => <div>shoelaces!</div>,
-    main: () => <h2>Shoelaces</h2>
+  { path: '/signup',
+    main: Signup
+  },
+  {
+    path:'*',
+    main: NotFound
   }
 ]
 
 const SidebarExample = () => (
-  <Router>
+  <Router history={browserHistory}>
     <div>
       <nav>
         <ul>
           <li><Link to="/">Home</Link></li>
-          <li><Link to="/bubblegum">Bubblegum</Link></li>
-          <li><Link to="/shoelaces">Shoelaces</Link></li>
+          <li><Link to="/login">Login</Link></li>
+          <li><Link to="/signup">Sign Up</Link></li>
         </ul>
       </nav>
       <div>
-        {routes.map((route, index) => (
-          // Render more <Route>s with the same paths as
-          // above, but different components this time.
-          <Route
-            key={index}
-            path={route.path}
-            exact={route.exact}
-            component={route.main}
-          />
-        ))}
+        <Switch>
+          {routes.map((route, index) => (
+            // Render more <Route>s with the same paths as
+            // above, but different components this time.
+            <Route
+              key={index}
+              path={route.path}
+              exact={route.exact}
+              component={route.main}
+            />
+          ))}
+        </Switch>
       </div>
     </div>
   </Router>
