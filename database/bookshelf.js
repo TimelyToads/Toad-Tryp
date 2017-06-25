403ce641cb8355cdc99c6a792ce0d8ad7eacc458
Dylan Gould <dylangood@gmail.com>
2017-06-25 14:55:34 -0700

    Route GET & POST requests to user(s) & trip(s)
    SO EASY with Bookshelf!!!

--- database/bookshelf.js
+++ database/bookshelf.js
@@ -1,18 +1,17 @@
 // The Bookshelf library is initialized by passing an initialized Knex client instance. 
 // The knex documentation provides a number of examples for different databases.
 // http://knexjs.org/
-const dbConfig;
-const knex = require('knex')(dbConfig);
 
-dbConfig = {
+const dbConfig = {
   client: 'mysql',
   connection: {
-    host : '127.0.0.1',
-    user : 'root',
-    password : '',
-    database : 'toads'
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'toads'
   }
 };
+const knex = require('knex')(dbConfig);
 
 // This initialization should likely only ever happen once in your application. 
 // As it creates a connection pool for the current database, 
@@ -21,35 +20,10 @@ dbConfig = {
 // somewhere in the application so you can reference it.
 
 const bookshelf = require('bookshelf')(knex);
-module.exports = bookshelf;
 
 // elsewhere, to use the bookshelf client:
 
 // var bookshelf = require('./bookshelf');
 // * * * TEMPLATE BOOKSHELF TABLE INSTANCES OF OUR DATABASE * * * // 
 
-const Trips = bookshelf.Model.extend({
-  tableName: 'trips',
-  users: function() {
-    return this.hasMany(Users);
-  }
-});
-
-
-const TripsToads = bookshelf.Model.extend({
-  tableName: 'trips_toads',
-  trips: function() {
-    return this.belongsToMany(Trips);
-  },
-  users: function() {
-    return this.hasMany(Users);
-  }
-});
-
-const Users = bookshelf.Model.extend({
-  tableName: 'users',
-  trips: function() {
-    return this.belongsToMany(Trips);
-  }
-})
-
+module.exports = bookshelf;
