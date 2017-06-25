1625276f9eb0b8b725b68cf1c722a7641e3423d6
fishboy <dylangood@users.noreply.github.com>
2017-06-24 19:56:40 -0700

    Update index.js
    Added comment for future reference about database endpoints

--- server/index.js
+++ server/index.js
@@ -11,6 +11,9 @@ app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+//ALL REST ENDPOINTS SHOULD START WITH /api/<YOUR PATH>
+//AND BE ABOVE THE FOLLOWING: app.get('/*'...)
+
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname + '/../client/dist/index.html'));
 })
