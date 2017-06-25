54043a231b75c1cd03a3b608bcc0f56125c8d9ec
Solomon Tang <solomoncztang@gmail.com>
2017-06-24 18:18:51 -0700

    Added NavBar that successfully changes URL and view upon clicking Link

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,33 +1,55 @@
-import React from 'react';
+import React from 'react'
 import {
   BrowserRouter as Router,
   Route,
   Link
-} from 'react-router-dom';
+} from 'react-router-dom'
 
+import Home from './Home.jsx';
+// Each logical "route" has two components, one for
+// the sidebar and one for the main area. We want to
+// render both of them in different places when the
+// path matches the current URL.
 const routes = [
-  {
-    path: '/',
+  { path: '/',
     exact: true,
+    sidebar: () => <div>home!</div>,
+    main: Home
   },
-  {
-    path: '/login'
+  { path: '/bubblegum',
+    sidebar: () => <div>bubblegum!</div>,
+    main: () => <h2>Bubblegum</h2>
   },
-  {
-    path: '/signup'
+  { path: '/shoelaces',
+    sidebar: () => <div>shoelaces!</div>,
+    main: () => <h2>Shoelaces</h2>
   }
-];
+]
 
-const NavBar = () => (
+const SidebarExample = () => (
   <Router>
     <div>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/signup">Sign Up</Link></li>
-      </ul>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/bubblegum">Bubblegum</Link></li>
+          <li><Link to="/shoelaces">Shoelaces</Link></li>
+        </ul>
+      </nav>
+      <div>
+        {routes.map((route, index) => (
+          // Render more <Route>s with the same paths as
+          // above, but different components this time.
+          <Route
+            key={index}
+            path={route.path}
+            exact={route.exact}
+            component={route.main}
+          />
+        ))}
+      </div>
     </div>
   </Router>
-);
+)
 
-export default NavBar;
+export default SidebarExample
---
+++ client/src/components/Signup.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Signup = () => (
+  <div>
+    <h1>
+      Signup Page!
+    </h1>
+  </div>
+);
+
+export default Signup;
