97c398aa058b97d207c7382c51bf02fa904de374
Jun Park <jun.park@live.com>
2017-06-26 12:15:15 -0700

    Renders basic ResultListItem when ResultsList gets passed a prop from the App level. Trips endpoint in the server only searches based on Departure City and Arrival City, needs refactoring.

--- client/src/components/SearchResults.jsx
+++ client/src/components/SearchResults.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import SearchResultsItem from './SearchResultsItem.jsx'
-
 import Search from './Search.jsx';
 
 //Resulting trips array can be found at props.location.state.trips
@@ -9,7 +8,8 @@ const SearchResults = (props) => (
   <div>
     <h1>SEARCH RESULTS!</h1>
       <Search />
-    {console.log(props.location.state)}
+      {(Array.isArray(props.location.state.trips)) ? props.location.state.trips.map(trip => <SearchResultsItem trip={trip} />) : <SearchResultsItem trip={props.location.state.trips} />}
+      {console.log(props.location.state.trips)}
   </div>
 );
 
--- server/index.js
+++ server/index.js
@@ -13,16 +13,17 @@ app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// app.get('/users', (req, res) => {
-//   console.log('GET /users endpoint pinged.');
-//   models.Users.fetch().then( (users) => {
-//     res.status(200).send(users);
-//   })
-//     .catch( (err) => {
-//       console.log('ERROR GETting Users collection: ', err);
-//       res.status(404).send(err);
-//     });
-// });
+app.get('/users', (req, res) => {
+  console.log('GET /users endpoint pinged.');
+  models.Users.fetch().then( (users) => {
+    res.status(200).send(users);
+  })
+    .catch( (err) => {
+      console.log('ERROR GETting Users collection: ', err);
+      res.status(404).send(err);
+    });
+});
+
 
 // app.get('/trips', (req, res) => {
 //   console.log('GET /trips endpoint pinged.');
@@ -30,58 +31,57 @@ app.use(bodyParser.json());
 //     .then( (trips) => {
 //       res.status(200).send(trips);
 //     })
-//     .catch( (err) => {
-//       console.log('ERROR GETting Trips collection: ', err);
-//       res.status(404).send(err);
-//     });
+    // .catch( (err) => {
+    //   console.log('ERROR GETting Trips collection: ', err);
+    //   res.status(404).send(err);
+    // });
 // });
 
-// app.post('/user', (req, res) => {
-//   let user = req.body;
-//   console.log('POSTing user data: ', user);
-//   new models.User(user).save()
-//     .then( (user) => {
-//       res.status(201).send(user);
-//     })
-//     .catch( (err) => {
-//       console.log('ERROR POSTing User model: ', err);
-//       res.status(400).send(err);
-//     });
-// });
+app.post('/user', (req, res) => {
+  let user = req.body;
+  console.log('POSTing user data: ', user);
+  new models.User(user).save()
+    .then( (user) => {
+      res.status(201).send(user);
+    })
+    .catch( (err) => {
+      console.log('ERROR POSTing User model: ', err);
+      res.status(400).send(err);
+    });
+});
 
-// app.post('/trip', (req, res) => {
-//   let trip = req.body;
-//   console.log('POSTing trip data: ', trip);
-//   new models.Trip(trip).save()
-//     .then( (trip) => {
-//       res.status(201).send(trip);
-//     })
-//     .catch( (err) => {
-//       console.log('ERROR POSTing Trip model: ', err);
-//       res.status(400).send(err);
-//     });
-// });
+app.post('/trip', (req, res) => {
+  let trip = req.body;
+  console.log('POSTing trip data: ', trip);
+  new models.Trip(trip).save()
+    .then( (trip) => {
+      res.status(201).send(trip);
+    })
+    .catch( (err) => {
+      console.log('ERROR POSTing Trip model: ', err);
+      res.status(400).send(err);
+    });
+});
 
 //ALL REST ENDPOINTS SHOULD START WITH /api/<YOUR PATH>
 //AND BE ABOVE THE FOLLOWING: app.get('/*'...)
 app.get('/trips', (req, res) => {
-  console.log('this is getting to the server', req.query)
-
-  // model.where({favorite_color: 'red', shoe_size: 12}).fetch().then(function() { //...
   Trips.query((qb) => {
-    qb.innerJoin('Users', 'Users.id', '=', 'Trips.driver_id');
+    // qb.innerJoin('Users', 'Users.id', '=', 'Trips.driver_id');
     qb.where({
       departure_city: req.query.depart,
       arrival_city: req.query.arrive,
     });
-  }).fetch()
-    .then((trips) => {
-      console.log(trips)
-      res.status(200).send(JSON.stringify(trips));
-    })
-    .catch((err) => {
-      res.status(404).send(err);
-    })
+  })
+  .fetch()
+  .then((trips) => {
+    console.log(trips)
+    res.status(200).send(JSON.stringify(trips));
+  })
+  .catch( (err) => {
+    console.log('ERROR GETting Trips collection: ', err);
+    res.status(404).send(err);
+  });
 });
 
 app.get('/*', (req, res) => {
