ca3e6774e364bd5ee84a7189f61100d57dc89b63
Solomon Tang <solomoncztang@gmail.com>
2017-06-26 17:15:46 -0700

    WARNING! ALL BACKEND API CALLS NOW BEGIN WITH /api/<YOUR_OLD_PATH> Fixed nested Routes causeing Unexpected Token < error

--- server/index.js
+++ server/index.js
@@ -37,7 +37,7 @@ app.get('/users', (req, res) => {
     // });
 // });
 
-app.post('/user', (req, res) => {
+app.post('/api/user', (req, res) => {
   let user = req.body;
   console.log('POSTing user data: ', user);
   new models.User(user).save()
@@ -50,7 +50,7 @@ app.post('/user', (req, res) => {
     });
 });
 
-app.post('/trip', (req, res) => {
+app.post('/api/trip', (req, res) => {
   let trip = req.body;
   console.log('POSTing trip data: ', trip);
   new models.Trip(trip).save()
@@ -63,9 +63,7 @@ app.post('/trip', (req, res) => {
     });
 });
 
-//ALL REST ENDPOINTS SHOULD START WITH /api/<YOUR PATH>
-//AND BE ABOVE THE FOLLOWING: app.get('/*'...)
-app.get('/trips', (req, res) => {
+app.get('/api/trips', (req, res) => {
   Trips.query((qb) => {
     // qb.innerJoin('Users', 'Users.id', '=', 'Trips.driver_id');
     qb.where({
@@ -83,6 +81,8 @@ app.get('/trips', (req, res) => {
     res.status(404).send(err);
   });
 });
+//ALL REST ENDPOINTS SHOULD START WITH /api/<YOUR PATH>
+//AND BE ABOVE THE FOLLOWING: app.get('/*'...)
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname + '/../client/dist/index.html'));
