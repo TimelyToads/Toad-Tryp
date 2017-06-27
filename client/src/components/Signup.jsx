c5526673e3404c562ca95aaa11ab1ebea63606a3
Jun Park <jun.park@live.com>
2017-06-27 15:49:19 -0700

    Structure for Signup page finished

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
 
-const Signup = () => (
-  <div>
-    <h1>
-      Signup Page!
-    </h1>
-  </div>
-);
+class Signup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      driver: false,
+
+    }
+  }
+
+
+  render() {
+    return (
+      <div className="signup-page">
+        <div>
+          <h1>
+            Signup for ToadTryp
+          </h1>
+
+          <p>Sign up with Google</p>
+          <p> or </p>
+          <form className="signup-form">
+            <input placeholder="E-mail Address"/><br/>
+            <input placeholder="First name"/><br/>
+            <input placeholder="Last name"/><br/>
+            <input placeholder="Phone number"/><br/>
+            <input placeholder="Image URL (optional)"/><br/>
+            <input placeholder="Create a Password"/><br/>
+
+            <button type="submit">Sign Up</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Signup;
