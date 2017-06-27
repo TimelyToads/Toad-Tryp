6d397074155ef1276da94f344395fbc9b9e4fe68
Solomon Tang <solomoncztang@gmail.com>
2017-06-26 18:15:37 -0700

    Changed case for import file

--- client/src/components/SearchResults.jsx
+++ client/src/components/SearchResults.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Redirect from 'react-router-dom/Redirect'
+import Redirect from 'react-router-dom/redirect'
 
 import SearchResultItem from './SearchResultItem.jsx'
 
