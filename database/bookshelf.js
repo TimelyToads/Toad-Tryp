71d4023f8c5ca51c213515d16aa83ad84aea4684
Solomon Tang <solomoncztang@gmail.com>
2017-06-28 16:33:14 -0700

    Add shortcircuit for prod/dev connections

--- database/bookshelf.js
+++ database/bookshelf.js
@@ -1,11 +1,19 @@
 // The Bookshelf library is initialized by passing an initialized Knex client instance. 
 // The knex documentation provides a number of examples for different databases.
 // http://knexjs.org/
+
+const local = {
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'toads'
+};
 const dbConfig = {
   client: 'mysql',
-  connection: process.env.DATABASE_URL
+  connection: process.env.DATABASE_URL || local
 };
 const knex = require('knex')(dbConfig);
+
 
 // This initialization should likely only ever happen once in your application. 
 // As it creates a connection pool for the current database, 
