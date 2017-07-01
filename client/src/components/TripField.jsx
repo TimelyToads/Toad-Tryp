ad485488fe2f3b4e79d2c61b85450d96b61104f9
Solomon Tang <solomoncztang@gmail.com>
2017-06-30 17:59:37 -0700

    Fluid input fields

--- client/src/components/TripField.jsx
+++ client/src/components/TripField.jsx
@@ -18,9 +18,10 @@ const TripField = (props) => (
           <Form.Field name='departure_city' control={Input} label='Depart City' placeholder='Depart City' onChange={props.handleChange}/>
         </Form.Group>
         <Form.Group widths='equal'>
-          <Form.Field name='departure_state' control={Input} label='Depart State' placeholder='Depart State' onChange={props.handleChange}/>
-          <Form.Field name='departure_zip' control={Input} label='Depart Zip' placeholder='Depart Zip' onChange={props.handleChange}/>
-          <Form.Field name='departure_date' control={Input} label='Depart Date' placeholder='Depart Date' onChange={props.handleChange}/>
+          <Form.Field fluid name='departure_state' control={Input} label='Depart State' placeholder='Depart State' onChange={props.handleChange}/>
+          <Form.Field fluid name='departure_zip' control={Input} label='Depart Zip' placeholder='Depart Zip' onChange={props.handleChange}/>
+          <Form.Field fluid name='departure_date' control={Input} label='Depart Date' placeholder='Depart Date' onChange={props.handleChange}/>
+          <Form.Field fluid name='departure_time' control={Input} label='Depart Time' placeholder='Depart Time' onChange={props.handleChange}/>
         </Form.Group>
         <Form.Group>
           <Form.Field name='seats' control={Select} label='Seats' options={options} onChange={props.handleChange}/>
@@ -33,9 +34,10 @@ const TripField = (props) => (
           <Form.Field name='arrival_city' control={Input} label='Arrive City' placeholder='Arrive City' onChange={props.handleChange}/>
         </Form.Group>
         <Form.Group widths='equal'>
-          <Form.Field name='arrival_state' control={Input} label='Arrive State' placeholder='Arrive State' onChange={props.handleChange}/>
-          <Form.Field name='arrival_zip' control={Input} label='Arrive Zip' placeholder='Arrive Zip' onChange={props.handleChange}/>
-          <Form.Field name='arrival_date' control={Input} label='Arrive Date' placeholder='Arrive Date' onChange={props.handleChange}/>
+          <Form.Field fluid name='arrival_state' control={Input} label='Arrive State' placeholder='Arrive State' onChange={props.handleChange}/>
+          <Form.Field fluid name='arrival_zip' control={Input} label='Arrive Zip' placeholder='Arrive Zip' onChange={props.handleChange}/>
+          <Form.Field fluid name='arrival_date' control={Input} label='Arrive Date' placeholder='Arrive Date' onChange={props.handleChange}/>
+          <Form.Field fluid name='arrival_time' control={Input} label='Arrive Time' placeholder='Arrive Time' onChange={props.handleChange}/>
         </Form.Group>
       </Segment>
     </Form>
