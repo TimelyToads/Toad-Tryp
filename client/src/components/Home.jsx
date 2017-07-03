fc955250e081781abb45b8436e34013a605cb0a9
Jun Park <jun.park@live.com>
2017-07-02 19:20:36 -0700

    Add FeaturedDestinations component on Home component that will automatically redirect a user to a search result by destination.

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { Container, Header } from 'semantic-ui-react';
 import Search from './Search/Search.jsx';
 import { withRouter } from 'react-router';
+import FeaturedDestinations from './FeaturedDestinations.jsx';
 
 
 class Home extends React.Component {
   constructor(props) {
     super(props);
   }
-  
+
   render() {
     return (
     <Container>
@@ -19,6 +20,7 @@ class Home extends React.Component {
         Go anywhere with a fellow Toad.
       </Header>
       <Search currentUser={this.props.currentUser} />
+      <FeaturedDestinations />
     </Container>
     );
   }
